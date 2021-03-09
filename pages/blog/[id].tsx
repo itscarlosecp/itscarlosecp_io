8a5d51b3b8dcb2da54d850a3f4b3acf5ae280f6a
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-09 00:43:28 -0300

    Changed some things with highlight

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -2,14 +2,19 @@ import type { InferGetStaticPropsType, GetStaticPaths } from 'next'
 import * as React from 'react'
 import PostLayout from '@layouts/Post'
 import { getPostBySlug, getPosts } from '@lib/db'
+import hljs from 'highlight.js'
 import highlight from '@lib/highlight'
 
 const Post = (post: InferGetStaticPropsType<typeof getStaticProps>) => {
+	const [mounted, setMounted] = React.useState(false)
+
 	React.useEffect(() => {
 		highlight()
+		hljs.highlightAll()
+		setMounted(true)
 	}, [])
 
-	return <PostLayout {...post} />
+	return mounted ? <PostLayout {...post} /> : <div></div>
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
