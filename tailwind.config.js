10be7d7fa023d6441e9d01d3ca06671e4c34479e
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-09 00:38:12 -0300

    Hopefully fixed style issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ const colors = require('tailwindcss/colors')
 const { spacing, fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./layouts/**/*.{js,ts,jsx,tsx}',
+	],
 	darkMode: 'class',
 	theme: {
 		extend: {
