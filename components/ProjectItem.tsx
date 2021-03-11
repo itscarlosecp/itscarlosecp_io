fdf1556397c2cfd37730be540e0d6cd227cd22d9
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-11 01:56:12 -0300

    Changed projects layout again, removed icon

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -10,23 +10,13 @@ const ProjectItem = (project: Project) => {
 			target='_blank'
 			rel='noopener noreferrer'
 		>
-			<div className='grid grid-cols-10 gap-2 border border-gray-200 dark:border-gray-800 rounded p-4'>
-				<div className='w-full h-full flex justify-center items-center dark:text-white'>
-					{project.iconId === 'next-js' && (
-						<Icons.Pen className='h-8 w-8 fill-current' />
-					)}
-					{project.iconId === 'vscode' && (
-						<Icons.Cpu className='h-8 w-8 fill-current' />
-					)}
-				</div>
-				<div className='col-span-8'>
-					<h4 className='text-lg font-bold tracking-tight text-gray-900 dark:text-gray-100'>
-						{project.name}
-					</h4>
-					<p className='leading-5 text-gray-700 dark:text-gray-300'>
-						{project.description}
-					</p>
-				</div>
+			<div className='flex flex-col gap-2 border border-gray-200 dark:border-gray-800 rounded p-4'>
+				<h4 className='text-lg font-bold tracking-tight text-gray-900 dark:text-gray-100'>
+					{project.name}
+				</h4>
+				<p className='leading-5 text-gray-700 dark:text-gray-300'>
+					{project.description}
+				</p>
 			</div>
 		</a>
 	)
