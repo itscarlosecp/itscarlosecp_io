5c572d191ad625d6d3e83301af7eb7540a77cd55
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-11 18:00:22 -0300

    Think Im done for today

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,7 +45,10 @@ module.exports = {
 						'h2,h3,h4': {
 							'scroll-margin-top': spacing[32],
 						},
-						figcaption: { color: colors.gray['400'] },
+						figcaption: {
+							color: colors.gray['500'],
+							'text-align': 'center',
+						},
 						'blockquote p:first-of-type::before': false,
 						'blockquote p:last-of-type::after': false,
 					},
