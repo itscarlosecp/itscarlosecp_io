4f0b10004ffae66bc31f8aee47f00afeaf1c2ac5
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-20 00:28:45 -0300

    Added preact

--- next.config.js
+++ next.config.js
@@ -10,4 +10,20 @@ module.exports = withPWA({
 			'avatars.githubusercontent.com',
 		],
 	},
+	webpack: (config, { dev, isServer }) => {
+		// if (isServer) {
+		// 	require('./scripts/generate-sitemap')
+		// 	require('./scripts/generate-rss')
+		// }
+
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+			})
+		}
+
+		return config
+	},
 })
