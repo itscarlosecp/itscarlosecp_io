86eb96900e8d0b9897e685647b820b9c0ad3f279
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-04-12 01:35:31 -0300

    Update subscribe.js

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,25 +1,33 @@
-import mailchimp from '@mailchimp/mailchimp_marketing';
+import mailchimp from '@mailchimp/mailchimp_marketing'
 
 mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_API_SERVER // e.g. us1
-});
+	apiKey: process.env.MAILCHIMP_API_KEY,
+	server: process.env.MAILCHIMP_API_SERVER,
+})
 
 export default async (req, res) => {
-  const { email } = req.body;
+	if (!req.body.email) {
+		return res.status(400).json({ error: 'Email is required' })
+	}
 
-  if (!email) {
-    return res.status(400).json({ error: 'Email is required' });
-  }
+	try {
+		const APIres = await mailchimp.lists.addListMember(
+			process.env.MAILCHIMP_AUDIENCE_ID,
+			{
+				email_address: req.body.email,
+				status: 'subscribed',
+			}
+		)
 
-  try {
-    await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
-      email_address: email,
-      status: 'subscribed'
-    });
+		return res.status(201).json({ error: '' })
+	} catch (err) {
+		const errorMsg =
+			err.response.body.title === 'Member Exists'
+				? err.response.body.detail.split('. ')[0] + '.'
+				: err.message || err.toString()
 
-    return res.status(201).json({ error: '' });
-  } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
-  }
-};
+		return res
+			.status(500)
+			.json({ error: `Error ${err.status}! ${errorMsg}` })
+	}
+}
