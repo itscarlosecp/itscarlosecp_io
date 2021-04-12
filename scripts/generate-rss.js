b0017699765eab2e591c13cbce22c15d6760e017
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-04-12 01:40:37 -0300

    Successfully added rss gen

--- scripts/generate-rss.js
+++ scripts/generate-rss.js
@@ -2,7 +2,7 @@ const { promises: fs } = require('fs')
 const path = require('path')
 const RSS = require('rss')
 const matter = require('gray-matter')
-const siteMetadata = require('@/data/siteMetadata')
+const siteMetadata = require('../data/siteMetadata')
 
 const generate = async () => {
 	const feed = new RSS({
