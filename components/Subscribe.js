cd9ad64f19213ccfd99d6c105b1ba59c75ad1fa5
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-04-12 01:35:11 -0300

    Update Subscribe.js

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -29,23 +29,33 @@ const Subscribe = () => {
 	}
 
 	return (
-		<form onSubmit={subscribe}>
-			<label htmlFor='email-input'>{'Email Address'}</label>
-			<input
-				id='email-input'
-				name='email'
-				placeholder='you@awesome.com'
-				ref={inputEl}
-				required
-				type='email'
-			/>
-			<div>
-				{message
-					? message
-					: `I'll only send emails when new content is posted. No spam.`}
-			</div>
-			<button type='submit'>{'✨ Subscribe 💌'}</button>
-		</form>
+		<div className='w-full p-6 bg-blue-50 border border-blue-200 rounded'>
+			<p className='text-xl font-bold text-blue-900'>
+				Subscribe to my newsletter
+			</p>
+			<p className='mt-1 text-gray-800'>
+				Stay tuned! I'll only send emails when new content is posted or
+				when new features are dropped. No spam.
+			</p>
+			<form onSubmit={subscribe} className='mt-4 flex gap-2'>
+				<input
+					id='email-input'
+					name='email'
+					placeholder='you@awesome.com'
+					className='flex-auto px-4 py-2 rounded'
+					ref={inputEl}
+					required
+					type='email'
+				/>
+				<button
+					type='submit'
+					className='px-4 bg-gray-200 rounded font-bold text-black'
+				>
+					Subscribe
+				</button>
+			</form>
+			{message && <div className='mt-4'>{message}</div>}
+		</div>
 	)
 }
 
